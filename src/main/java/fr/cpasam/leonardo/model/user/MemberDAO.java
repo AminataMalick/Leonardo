810181db953bdbac95dd82fd3a474c0409178f98
celine <bottece@gmail.com>
2018-05-04 15:42:19 +0200

    MembersDAO test

--- src/main/java/fr/cpasam/leonardo/model/user/MemberDAO.java
+++ src/main/java/fr/cpasam/leonardo/model/user/MemberDAO.java
@@ -1,19 +1,29 @@
 package fr.cpasam.leonardo.model.user;
+import java.util.ArrayList;
+import java.util.List;
 
-
-import java.util.logging.Logger;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
 import fr.cpasam.leonardo.utilities.CRUD_Service;
+import fr.cpasam.leonardo.utilities.HibernateUtil;
 
 
 
-public abstract class MemberDAO <T> implements CRUD_Service <T> {
-	private Class<T> persistentClass;
-    private Session session;
-    private static SessionFactory sessionFactory;
-    private static Logger LOGGER = Logger.getLogger("InfoLogging");
+public abstract class MemberDAO <T> implements CRUD_Service <Member> {
 
+	public List<Member> getAllMembers() {
+
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    session.beginTransaction();
+
+   
+    List<Member> myList = new ArrayList<Member>();
+    Query query = session.getNamedQuery("findAllMembers");
+    myList = query.list();
+   
+    session.getTransaction().commit();  
+ return myList ;   
+	}
 }
-
