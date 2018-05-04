94ec39c8c78be059728d2aca88016203cb9977a3
MelanieCha <melanie.chasane@gmail.com>
2018-05-04 10:48:34 +0200

    initialisation resources

---
+++ src/main/java/fr/cpasam/leonardo/resources/ShopRessource.java
@@ -0,0 +1,56 @@
+package fr.cpasam.leonardo.resources;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import fr.cpasam.leonardo.model.shop.Shop;
+
+@Path("shops/")
+public class ShopRessource {
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Shop> getAllShops() {
+    	
+    	List<Shop> shops = new ArrayList<>();
+    	
+    	// construction d'un entity manager
+    	EntityManager em =  ;
+
+    	 // récupération d'une requête nommée par interrogation 
+    	 // de l'entity manager
+    	Query marinByName = em.createNamedQuery("Marin.findByName") ;
+
+    	 // paramétrage de la requête
+    	marinByName.setParameter("name",  "Surcouf") ;
+
+    	 // exécution de la requête
+    	List<Marin> marins = marinByName.getResultList() ;
+    	
+    	//
+    	
+    	shops.add(shop);
+
+        return shops;
+    }
+    
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Shop getShop(@PathParam("id") long id) {
+
+    	Shop shop = new Shop(id);
+    	shop.text = "Shop";
+
+        return shop;
+    }
+}
