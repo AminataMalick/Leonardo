8451255367266e6d87351be87790068c0ed7e60f
MelanieCha <melanie.chasane@gmail.com>
2018-05-04 15:43:54 +0200

    Gestion ressource step 3

--- src/main/java/fr/cpasam/leonardo/resources/ShopRessource.java
+++ src/main/java/fr/cpasam/leonardo/resources/ShopRessource.java
@@ -1,53 +1,85 @@
 package fr.cpasam.leonardo.resources;
 
+import java.util.function.Predicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import fr.cpasam.leonardo.model.shop.Shop;
 
-@Path("shops/")
+@Path("shop/")
 public class ShopRessource {
-    @GET
+    /*
+	@GET
     @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Shop> getAllShops() {
-    	
-    	List<Shop> shops = new ArrayList<>();
-    	
-    	EntityManagerFactory factory = Persistence.createEntityManagerFactory();
-    	EntityManager em = factory.createEntityManager();
+    public List<Shop> resGetAllShops() {
 
-    	Query shopsAll = em.createNamedQuery("Shop.findAllShops");
-
-    	 // exécution de la requête
-    	shops = shopsAll.getResultList() ;
-    	
-    	
+    	List<Shop> shops = new ArrayList<>() ;
+    	shops = getAllShops();
         return shops;
     }
+    */
     
-    
-    /*
+/*
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Shop getShop(@PathParam("id") long id) {
+    public Shop resGetShopID(@PathParam("id") long id) {
 
-    	Shop shop = new Shop(id);
-    	shop.text = "Shop";
-
+    	Shop shop = new getShopID(id);
         return shop;
     }
     */
+    
+    
+    /*
+    @POST
+    @Path("/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addShop(Shop s){
+      cList.add(s);
+      return Response.status(201).build();
+    }  
+    
+    
+    @PUT
+    @Path("{id}/update")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateShop(Shop s){
+      int matchIdx = 0;
+      Optional<Shop> match = cList.stream()
+          .filter(c -> c.getId() == customer.getId())
+          .findFirst();
+      if (match.isPresent()) {
+        matchIdx = cList.indexOf(match.get());
+        cList.set(matchIdx, s);
+        return Response.status(Response.Status.OK).build();
+      } else {
+        return Response.status(Response.Status.NOT_FOUND).build();      
+      }
+    }  
+    
+    
+    @DELETE
+    @Path("/remove/{id}")
+    public void deleteShop(@PathParam("id") long id){
+      Predicate <Shop> shop = c -> c.getId() == id;
+      if (!cList.removeIf(shop)) {
+       throw new NotFoundException(new JsonError("Error", "Customer " + id + " not found"));
+      }
+    }  
+    */
+
 }
