5dc760d1f62480026714df9d8d3e39d6ad38dd7e
celine <bottece@gmail.com>
2018-05-06 10:53:36 +0200

    requete chercher tous les membres + debut creation membre non fini

--- src/main/java/fr/cpasam/leonardo/model/user/MemberDAO.java
+++ src/main/java/fr/cpasam/leonardo/model/user/MemberDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
+import fr.cpasam.leonardo.model.Prout;
 import fr.cpasam.leonardo.utilities.CRUD_Service;
 import fr.cpasam.leonardo.utilities.HibernateUtil;
 
@@ -29,8 +30,15 @@ public class MemberDAO implements CRUD_Service <Member> {
 
 	@Override
 	public void Create(Member entity) {
-		// TODO Auto-generated method stub
-		
+		  Session session = HibernateUtil.getSessionFactory().openSession();
+		  session.beginTransaction();
+
+		  Member member = new Member();
+		//	member.id 
+		    
+        //member. = "WOW!";
+       // session.save(member);
+        session.getTransaction().commit();
 	}
 
 	@Override
