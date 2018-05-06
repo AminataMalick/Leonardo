6a8142196a9ad2aff1a868adf6e21854cb755ade
Pierre Belabbes <pbelabbes@gmail.com>
2018-05-06 23:08:59 +0200

    clean model

--- src/main/java/fr/cpasam/leonardo/model/chat/Message.java
+++ src/main/java/fr/cpasam/leonardo/model/chat/Message.java
@@ -1,15 +1,5 @@
 package fr.cpasam.leonardo.model.chat;
 
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 import fr.cpasam.leonardo.model.user.Member;
 
 import java.util.List;
@@ -17,26 +7,16 @@ import java.util.List;
 import javax.persistence.Column;
 
 
-@Entity 
-@Table (name = "message")
 public abstract class Message {
 
-	@Id
-	@Column (name ="MESSAGE_ID")
 	private long id ;
 	
-	@Column (name = "TRANSMITTER")
 	List<Member> transmitter;
 	
-	@Column (name = "DATE")
-	@Temporal(TemporalType.DATE)
 	private java.util.Date date;
 	
-	@Column (name = "CONTENT")
 	private String content ;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name="CHAT_ID")
 	private Chat chat;
 
 	public Message() {}
--- src/main/java/fr/cpasam/leonardo/model/tag/ProductTag.java
+++ src/main/java/fr/cpasam/leonardo/model/tag/ProductTag.java
@@ -1,34 +1,8 @@
 package fr.cpasam.leonardo.model.tag;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
-
 import fr.cpasam.leonardo.model.product.Product;
 
-@NamedQueries({
-	@NamedQuery(
-	name = ProductTag.FIND_PRODUCT_TAG_BY_ID,
-	query = "from ProductTag pt where pt.id = :tagId"
-	),
-	@NamedQuery(
-	name = ProductTag.FIND_ALL_PRODUCT_TAGS,
-	query = "from ProductTag"
-	)
-})
-@Entity
-@Table(name="product_tags")
-@PrimaryKeyJoinColumn(name="TAG_ID")
 public class ProductTag extends Tag{
-	public final static String FIND_PRODUCT_TAG_BY_ID = "findProductTagById";
-	public final static String FIND_ALL_PRODUCT_TAGS = "findAllProductTags";
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name="PRODUCT_ID")
 	protected Product product;
 
 	public ProductTag() {}
