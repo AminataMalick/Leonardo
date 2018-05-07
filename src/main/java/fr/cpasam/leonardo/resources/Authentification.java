c84a18c9310339036633338015e7ed96713a1000
Vondair <antoine.carrier@hotmail.fr>
2018-05-07 09:57:04 +0200

    fin de cette feature pour pull la fin d'hibernate et recommencer une autre feature

--- src/main/java/fr/cpasam/leonardo/resources/Authentification.java
+++ src/main/java/fr/cpasam/leonardo/resources/Authentification.java
@@ -7,15 +7,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
-import com.sun.jersey.json.impl.provider.entity.JSONObjectProvider;
-
 public class Authentification {
 	@Context private HttpServletRequest request;
 
 	@POST
 	@Path("/login")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void login(JSONObjectProvider json) {
+	public void login(Json json) {
 
 	    // Implementation of your authentication logic
 	    /*if (authenticate(username, password)) {
