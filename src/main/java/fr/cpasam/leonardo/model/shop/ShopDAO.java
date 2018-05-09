826fee7580c044d6e11ad2ab140940600fce383e
Aminata <aminata_b@rocketmail.com>
2018-05-09 10:18:58 +0200

    shopdao.java pas modifié mais à commit

--- src/main/java/fr/cpasam/leonardo/model/shop/ShopDAO.java
+++ src/main/java/fr/cpasam/leonardo/model/shop/ShopDAO.java
@@ -1,10 +1,26 @@
 package fr.cpasam.leonardo.model.shop;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.mysql.cj.api.jdbc.Statement;
+
 public class ShopDAO {
 
 	public Shop get(Long shopId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
+		try {
+			Statement statement = connexion.createStatement();
+	        /* Exécution d'une requête de lecture */
+			ResultSet resultat = statement.executeQuery( "SELECT * FROM Shop WHERE idShop="+ shopId+";" );
+	            
+	        	/* Récupération des données du résultat de la requête de lecture */
+	            while ( resultat.next() ) {
+	            	int idShop= resultat.getInt(1);
+	                String nameShop = resultat.getString(2);
+	                String descrShop = resultat.getString(3);
+	            } 
+		}catch (SQLException e) { e.printStackTrace();} 
+			return null;
+		}
 
 }
