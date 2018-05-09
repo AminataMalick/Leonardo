335808e0ce1976b8d5108008a1510b5362843523
celine <bottece@gmail.com>
2018-05-09 14:54:44 +0200

    Suite DAO User et Member

--- src/main/java/fr/cpasam/leonardo/model/user/MemberDAO.java
+++ src/main/java/fr/cpasam/leonardo/model/user/MemberDAO.java
@@ -6,25 +6,18 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import fr.cpasam.leonardo.utilities.DAOManager;
-
-
-
 
 
 public class MemberDAO extends DAOManager {
 
-	
-
-	
 	// Affichage de tous les membres de la BD
-	public static List<Member> All() {
+	public static List<Member> all() {
 		List<Member> members = new ArrayList<Member>();
 		Statement stmt = null;
 		try {
 			stmt = con.createStatement();
-			ResultSet rset = stmt.executeQuery("QUERY_FIND_MEMBERS");
+			ResultSet rset = stmt.executeQuery("SELECT * FROM Member natural join User");
 
 			while (rset.next()) {
 				Member member = new Member(rset.getInt(1),rset.getString(2),rset.getString(3),rset.getString(4),rset.getString(5));
@@ -43,41 +36,80 @@ public class MemberDAO extends DAOManager {
 					e.printStackTrace();
 				}
 			}
-
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 		}   
-
 		return members ;   
 	}
 
 
 
-
-	 
 	/**
-	 *  Créé un membre avec la recuperation des donnees du formulaire
+	 *  Créer un membre avec la recuperation des donnees du formulaire
 	 * @param id
 	 * @param firstName
 	 * @param lastName
 	 * @param email
 	 * @param pwd
 	 */
-	public void CreateMember(String firstName, String lastName, String email, String pwd ) {
-
+	public static void create(String firstName, String lastName, String email, String pwd ) {
 		Member member = new Member(firstName, lastName, email, pwd);
-	
-}
-	
-
-
-	public Member get(Long memberID) {
-		// TODO Auto-generated method stub
-		return null;
 	}
+
+
+	// Trouver un membre a l'aide de on ID
+	public static Member get(Long memberID) {
+		Statement stmt = null;
+		try {
+			stmt = con.createStatement();
+			ResultSet rset = stmt.executeQuery("SELECT * FROM Member natural join User WHERE id_User="+memberID);
+			if(rset.next())
+			{			
+				while (rset.next()) {
+					Member member = new Member(rset.getLong(1),rset.getString(2),rset.getString(3),rset.getString(4),rset.getString(5));
+					return member ;
+				}
+
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}	
+		return null ;
+	}
+	
+	
+	
+	public static void delete(Long memberID) {
+		Statement stmt = null;
+		try {
+			stmt = con.createStatement();
+			int deleted =stmt.executeUpdate("DELETE FROM ShopMember WHERE id_Member="+memberID);
+			stmt.executeUpdate("DELETE FROM Message WHERE id_Member="+memberID);
+			stmt.executeUpdate("DELETE FROM Shop WHERE id_Member="+memberID);
+			stmt.executeUpdate("DELETE FROM Admin WHERE id_User="+memberID);
+			stmt.executeUpdate("DELETE FROM Member WHERE id_Member="+memberID);
+			stmt.executeUpdate("DELETE FROM User WHERE id_User="+memberID);
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}	
+	}
+	
+	
+	
 }
