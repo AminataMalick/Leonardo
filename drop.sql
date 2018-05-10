18134c0d0e776bae77927c19e5f90143cceb9c7b
MelanieCha <melanie.chasane@gmail.com>
2018-05-10 11:22:07 +0200

    shopDAO fonction getMember d'un shop

---
+++ drop.sql
@@ -0,0 +1,15 @@
+drop table ShopMember ;
+drop table ProductTag ;
+drop table Tag ;
+drop table Product ;
+drop table ShopRetail ;
+drop table RetailPoint ;
+drop table Message ;
+drop table ShopChat ;
+drop table Shop ;
+drop table Admin ;
+drop table Member ;
+drop table User ;
+drop table Geoloc ;
+
+
--- src/main/java/fr/cpasam/leonardo/model/shop/ShopDAO.java
+++ src/main/java/fr/cpasam/leonardo/model/shop/ShopDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import java.sql.Statement;
 
-
+import fr.cpasam.leonardo.model.user.Member;
 import fr.cpasam.leonardo.utilities.DAOManager;
 
 public class ShopDAO extends DAOManager {
@@ -46,8 +46,22 @@ public class ShopDAO extends DAOManager {
 
 		return shops;
 	}
-	public static void getMember(long user_id) {
-		// TODO Auto-generated method stub
+	public static Member getMember(long member_id, long shop_id) {
+		Statement statement = null;		
+		Member member = null ;
+		try {
+			statement = con.createStatement();
+			/* Exécution d'une requête de lecture */
+			ResultSet resultat = statement.executeQuery( "SELECT * FROM User NATURAL JOIN Member WHERE id_Member="+ member_id + "= (SELECT id_Member FROM Shop WHERE id_Shop = " + shop_id + ")" );
+
+			/* Récupération des données du résultat de la requête de lecture */
+			while ( resultat.next() ) {
+				//int idShop= resultat.getInt(1);
+				member = new member(resultat.getLong(7),resultat.getString(2),resultat.getString(3),resultat.getString(4), resultat.getString(5), resultat.getString(6));
+			} 
+			
+		}catch (SQLException e) { e.printStackTrace();} 
+		return member;
 
 	}
 
