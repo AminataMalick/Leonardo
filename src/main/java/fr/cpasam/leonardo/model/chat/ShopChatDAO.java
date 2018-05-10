507e3b4008fddef8c001856e469eb04e125f6002
MelanieCha <melanie.chasane@gmail.com>
2018-05-10 18:38:19 +0200

    rien

--- src/main/java/fr/cpasam/leonardo/model/chat/ShopChatDAO.java
+++ src/main/java/fr/cpasam/leonardo/model/chat/ShopChatDAO.java
@@ -1,5 +1,9 @@
 package fr.cpasam.leonardo.model.chat;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import fr.cpasam.leonardo.model.shop.Shop;
 import fr.cpasam.leonardo.model.user.Member;
 import fr.cpasam.leonardo.utilities.DAOManager;
@@ -7,7 +11,7 @@ import fr.cpasam.leonardo.utilities.DAOManager;
 public class ShopChatDAO extends DAOManager {
 
 	public static ShopChat create(Member m, Shop s) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
