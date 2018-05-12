654aad193129204a1d8dfb5569661d659a110d31
MelanieCha <melanie.chasane@gmail.com>
2018-05-12 17:09:30 +0200

    ProductDAO et ProductTAGDAO ok et testé

--- src/main/java/fr/cpasam/leonardo/model/tag/ProductTagDAO.java
+++ src/main/java/fr/cpasam/leonardo/model/tag/ProductTagDAO.java
@@ -7,12 +7,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.cpasam.leonardo.model.product.Product;
+import fr.cpasam.leonardo.model.product.ProductDAO;
 import fr.cpasam.leonardo.model.shop.Shop;
 import fr.cpasam.leonardo.model.user.Member;
 import fr.cpasam.leonardo.utilities.DAOManager;
 
 public class ProductTagDAO extends DAOManager{
+	
+	/**
+	 * Ajoute un tag à un produit
+	 * @param product_id
+	 * @param tag_id
+	 * @return Product
+	 */
+	public static Product addTag(long product_id, long tag_id) {
+		
+		Statement statement = null;	
+		Product product = null ;
+		ArrayList<Tag> tags = new ArrayList<Tag>() ;
+		
 
+		try {
+			Tag tag = null ;
+			statement = con.createStatement();
+			/* Insertion d'un tag*/
+			statement.executeUpdate("INSERT INTO ProductTag(id_Product, id_Tag)VALUES("+product_id+","+tag_id+")");
+			
+			/* Récupération du produit */
+			product = ProductDAO.get(product_id);
+			
+			/* Récupération des infos du tag et ajout de celui */
+			tag = TagDAO.getTagByID(tag_id);
+			product.addTag(tag);
+			
+			
+		}catch (SQLException e) { e.printStackTrace();} 
+		try { statement.close();
+		} catch (SQLException e) { e.printStackTrace();}
+		return product;
+	}
+	
+	
 	/**
 	 * Retourne la liste de tags lié au produit donné
 	 * @param id
@@ -39,6 +74,32 @@ public class ProductTagDAO extends DAOManager{
 		return tags;
 	}
 
+	
+	public static ArrayList<Product> getProductsByTag(long tag_id) {
+		
+		ArrayList<Product> products = new ArrayList<Product>();
+		ArrayList<Tag> tags = new ArrayList<Tag>();
+
+		Statement statement = null;		
+
+		try {
+			Product product = null ;
+			statement = con.createStatement();
+			/* Exécution d'une requête de lecture */
+			ResultSet resultat = statement.executeQuery( "SELECT * FROM Product Natural Join ProductTag WHERE id_Tag="+tag_id);
+
+			/* Récupération des données du résultat de la requête de lecture */
+			while ( resultat.next() ) {
+				long product_id = resultat.getLong(1) ;
+				tags = getTagsByProduct(product_id) ;
+				product = new Product(product_id, resultat.getString(2), resultat.getLong(4), resultat.getFloat(3), tags);
+				products.add(product);
+			} 
+		}catch (SQLException e) { e.printStackTrace();} 
+		try { statement.close();
+		} catch (SQLException e) { e.printStackTrace();}
+		return products;
+	}
 	
 	
 	public static void delete(long tag_id) {
--- src/test/java/fr/cpasam/leonardo/productTagDAO/TestProductTagDAO.java
+++ src/test/java/fr/cpasam/leonardo/productTagDAO/TestProductTagDAO.java
@@ -2,10 +2,22 @@ package fr.cpasam.leonardo.productTagDAO;
 
 import java.util.ArrayList;
 
+import fr.cpasam.leonardo.model.product.Product;
+import fr.cpasam.leonardo.model.tag.ProductTagDAO;
+import fr.cpasam.leonardo.model.tag.Tag;
 import fr.cpasam.leonardo.model.tag.TagDAO;
 
 public class TestProductTagDAO {
 	public static void main(String[] args) {
+		
+		/*
+		// Test addTag
+		long product_id = 1;
+		long tag_id = 1;
+		Product product = null ;
+		product = ProductTagDAO.addTag(product_id, tag_id);
+		System.out.println(product);
+		*/
 		
 		/*
 		//Test delete
@@ -15,19 +27,19 @@ public class TestProductTagDAO {
 		*/
 		
 		/*
-		// Test getTagByName
-		ProductTag productTag = null ;
-		String keyword = "tomate" ;
-		productTag = ProductTagDAO.getTagByName(keyword);
-		System.out.println(productTag);
+		// Test getTagsByProduct
+		long id = 1 ;
+		ArrayList<Tag> tags = new ArrayList<Tag>();
+		tags = ProductTagDAO.getTagsByProduct(id);
+		System.out.println(tags);
 		*/
 		
 		/*
-		// Test getTagsByProduct
-		long id = 1 ;
-		ArrayList<ProductTag> tags = new ArrayList<ProductTag>();
-		tags = ProductTagDAO.getTagsByProduct(id);
-		System.out.println(tags);
+		//Test getProductsByTag
+		long tag_id = 1 ;
+		ArrayList<Product> products = new ArrayList<Product>() ;
+		products = ProductTagDAO.getProductsByTag(tag_id);
+		System.out.println(products);
 		*/
 		
 		/*
